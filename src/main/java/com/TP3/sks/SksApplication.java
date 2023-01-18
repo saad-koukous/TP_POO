fb8a40c1d6be23c9e4ac8d8c544725287cbd637f
Saad koukous <saad_koukous@um5.ac.ma>
2023-01-18 23:37:09 +0100

    first commit

---
+++ src/main/java/com/TP3/sks/SksApplication.java
@@ -0,0 +1,13 @@
+package com.TP3.sks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/TP3/sks/SksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TP3.sks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
